0dd343c509e61904eae5fbac0e22bff7d8a9175a
Victor Bianchi <vbianchi90@gmail.com>
2019-11-19 20:49:17 -0300

    add Dark Sky API call

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import DayList from '../DayList';
 
 import { Container } from './styles';
 
 export default function Weather() {
+  const [dailyForecast, setDailyForecast] = useState(null);
+  const [hourlyForecast, setHourlyForecast] = useState(null);
+
+  useEffect(() => {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY_API_KEY}/37.8267,-122.4233?exclude=currently,minutely,alerts,flags&units=si&extend=hourly`;
+    fetch(proxyUrl + targetUrl)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        // handle API data
+      });
+  }, []);
+
   return (
     <Container>
       <DayList />
